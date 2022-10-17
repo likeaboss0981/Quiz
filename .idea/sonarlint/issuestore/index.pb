b3731c5ae5b40ed356954b2046a3a6a12b8afc14
Michal <michalo080801@gmail.com>
2022-10-17 16:48:34 +0200

    Add new project

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
-m
-=app/src/main/java/com/example/myapplication/MainActivity.java,9\7\97d5d04e52812b9d0ac3b157695041e5f8fbc36a
-i
-9app/src/main/java/com/example/myapplication/Question.java,a\5\a5d814764fa5d117e0df538e8fc513ed2c1554b9
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,20 +17,34 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button promptButton;
     private TextView questionTextView;
 
     private int currentIndex = 0;
     private int score = 0;
     private int buttonClicked = 0;
 
+    private static final String TAG = "MainActivity";
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    public static final String KEY_EXTRA_ANSWER = "extraanswer";
+    private static final int REQUEST_CODE_PROMPT = 0;
+    private boolean answerWasShown;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"Wywołana została matoda życia :onCreate");
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
+        promptButton = findViewById(R.id.promptButton);
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
@@ -51,8 +70,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 currentIndex = (currentIndex + 1)%questions.length;
+                answerWasShown = false;
                 setNextQuestion();
 
+            }
+        });
+
+        promptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,PromptActivity.class);
+                boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+                intent.putExtra(KEY_EXTRA_ANSWER,correctAnswer);
+                startActivityForResult(intent,REQUEST_CODE_PROMPT);
             }
         });
         setNextQuestion();
@@ -66,17 +96,21 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.trojkat_rownoboczny,true)
     };
 
-    private void checkAnswerCorrectness(boolean userAnswer){
+    private void checkAnswerCorrectness(boolean userAnswer) {
         boolean correctAnswer = questions[currentIndex].isTrueAnswer();
         int resultMessageId = 0;
-        if(userAnswer == correctAnswer){
-            resultMessageId = R.string.correct_answer;
-            score++;
+        if (answerWasShown) {
+            resultMessageId = R.string.answer_was_shown;
         }
-        else{
-            resultMessageId = R.string.incorrect_answer;
+        else {
+            if (userAnswer == correctAnswer) {
+                resultMessageId = R.string.correct_answer;
+                score++;
+            } else {
+                resultMessageId = R.string.incorrect_answer;
+            }
         }
-        Toast.makeText(this,resultMessageId,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
     }
 
     private void setNextQuestion(){
@@ -84,6 +118,54 @@ public class MainActivity extends AppCompatActivity {
             finish();
         questionTextView.setText(questions[currentIndex].getQuestionId());
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode != RESULT_OK)
+            return;
+        if(requestCode == REQUEST_CODE_PROMPT){
+            if(data == null)
+                return;
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN,false);
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"Wywołana została matoda życia :onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"Wywołana została matoda życia :onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"Wywołana została matoda życia :onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"Wywołana została matoda życia :onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"Wywołana została matoda życia :onResume");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG,"Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
     }
 
     public void finish(){
